e33809fc9e1cf44c4dccd68015aa397d3e4cec19
scymz2 <mochuan.zhan@manchester.ac.uk>
2024-05-20 14:21:25 +0100

    zero depth list removed

--- submit_potholeAllv.sh
+++ submit_potholeAllv.sh
@@ -1,16 +1,13 @@
 #!/bin/bash --login
 #$ -cwd
-#$ -l v100           # A 1-GPU request (v100 is just a shorter name for nvidia_v100)
+#$ -l a100           # A 1-GPU request (v100 is just a shorter name for nvidia_v100)
                      # Can instead use 'a100' for the A100 GPUs (if permitted!)
-#$ -N pAllv_new       # job name
-#$ -pe smp.pe 4      # 4 CPU cores available to the host code
+#$ -N pAllv_new      # job name
+#$ -pe smp.pe 8      # 4 CPU cores available to the host code
                      # Can use up to 12 CPUs with an A100 GPU.
 #$ -o ~/scratch/DSNeRF/logs/output_Allv_new.txt
 #$ -e ~/scratch/DSNeRF/logs/error_Allv_new.txt
-
-module load libs/nvidia-hpc-sdk/23.9
-module load apps/binapps/anaconda3/2022.10
-source activate colmap
+#$ -V
 
 python imgs2poses.py data/potholeAllView/
 python run_nerf.py --config configs/pothole_Allv_new.txt
